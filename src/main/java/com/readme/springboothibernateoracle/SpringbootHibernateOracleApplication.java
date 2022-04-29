8010a364e1f2cac16aa3a1dff5bc861b283e4136
sangram <sangramjee22@gmail.com>
2022-04-29 08:50:10 +0530

    first commit

---
+++ src/main/java/com/readme/springboothibernateoracle/SpringbootHibernateOracleApplication.java
@@ -0,0 +1,13 @@
+package com.readme.springboothibernateoracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootHibernateOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHibernateOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readme/springboothibernateoracle/SpringbootHibernateOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readme.springboothibernateoracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootHibernateOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
